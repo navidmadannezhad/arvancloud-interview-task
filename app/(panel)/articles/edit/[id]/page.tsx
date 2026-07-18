4a780ba0aeca77a6b565ca43ad3c909dc186ec7e
navidmnzh <navidproject283@gmail.com>
2026-07-18 17:35:00 +0330

    fix: remove todo

--- app/(panel)/articles/edit/[id]/page.tsx
+++ app/(panel)/articles/edit/[id]/page.tsx
@@ -10,10 +10,3 @@ export default async function ArticleEdit({ params }: ArticleEditPageProps) {
   const { id } = await params
   return <ArticleForm articleID={parseInt(id)} />;
 }
-
-
-// TODO - find a way to overcome race condition for /me endpoint
-// TODO - check audit
-// TODO - check memory usage
-// TODO - set build strategy (standalone and etc)
-// TODO - dockerize
